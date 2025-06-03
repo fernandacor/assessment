bbaeb3819c513fb260192c4fde65fe06ab8f7b03
fernandacor <a01782232@tec.mx>
2025-06-02 22:11:02 -0600

    pagina code editor (jala al componente)

---
+++ frontend/src/app/codeEditor/page.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import CodeEditor from "@/components/CodeEditor";
+
+export default function Code_Editor() {
+  return (
+    <main>
+      <CodeEditor />
+    </main>
+  );
+}
