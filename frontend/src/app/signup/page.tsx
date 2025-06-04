1b48ecf3e6755bcb4f752d18f1b0d0098447c49f
Alina Rosas <a01252720@itesm.mx>
2025-06-03 20:25:23 -0600

    sign up

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function SignUp() {
   return (
     <div className='flex w-full flex-col px-6 py-12 bg-[url(/lightBG.jpg)] dark:bg-[url(/darkBG.jpg)] bg-cover h-screen' >
-      <div className='flex flex-col flex-initial w-3xl h-9/10 items-center bg-slate-50/50 rounded-xl mx-auto dark:bg-stone-700/25'>
+      <div className='flex flex-col flex-initial w-3xl h-full items-center bg-slate-50/50 rounded-xl mx-auto dark:bg-stone-700/25'>
       <div className='mx-auto w-full max-w-sm'>
         <h2 className='mt-10 text-center text-2xl font-light leading-9 tracking-tight text-lightest'>
           Sign Up for a New Account
