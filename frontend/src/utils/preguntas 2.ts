2871506fb39a247fe49e56a9bbd000e784aae84a
fernandacor <a01782232@tec.mx>
2025-06-05 18:28:35 -0600

    se crearon solos

---
+++ frontend/src/utils/preguntas 2.ts
@@ -0,0 +1,13 @@
+export const preguntas = [
+  'age',
+  'gender',
+  'academicLevel',
+  'country',
+  'dailyUsage',
+  'favPlatform',
+  'academicPerformance',
+  'sleepHours',
+  'mentalHealth',
+  'relationship',
+  'conflicts',
+]
