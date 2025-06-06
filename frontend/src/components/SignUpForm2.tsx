54c2b7786490c27a26ada1ae1756c0eb4a7ead63
Samuel Acevedo <a01026893@tec.mx>
2025-06-06 11:36:06 -0600

    Cambios de back pero en front

--- frontend/src/components/SignUpForm2.tsx
+++ frontend/src/components/SignUpForm2.tsx
@@ -7,10 +7,7 @@ interface FormValues {
   email: string;
   password: string;
   confirmPassword: string;
-  name: string;
-  surname: string;
-  address: string;
-  birthdate: string;
+  username: string;
 }
 
 export default function SignUpForm() {
@@ -20,10 +17,7 @@ export default function SignUpForm() {
     email: "",
     password: "",
     confirmPassword: "",
-    name: "",
-    surname: "",
-    address: "",
-    birthdate: "",
+    username: "",
   });
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -33,17 +27,14 @@ export default function SignUpForm() {
     }
 
     // → Aquí: llamamos a la ruta interna de Next
-    const res = await fetch("/api/auth/signup", {
+    const res = await fetch("/api/front/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: formValues.email,
         password: formValues.password,
         confirmPassword: formValues.confirmPassword,
-        name: formValues.name,
-        surname: formValues.surname,
-        address: formValues.address,
-        birthdate: formValues.birthdate,
+        username: formValues.username,
       }),
     });
 
@@ -106,9 +97,9 @@ export default function SignUpForm() {
           name="username"
           type="text"
           required
-          value={formValues.name}
+          value={formValues.username}
           onChange={(e) =>
-            setFormValues({ ...formValues, name: e.target.value })
+            setFormValues({ ...formValues, username: e.target.value })
           }
           className="block w-full px-3 py-2 mt-2 border rounded-md text-black bg-neutral-100/50"
         />
