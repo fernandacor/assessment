37aba2e7cdbd801e3ca0ceea881965e70326e626
fernandacor <a01782232@tec.mx>
2025-06-05 19:46:14 -0600

    academic level volvio

--- frontend/src/app/testQuests/academicLevel/page.tsx
+++ frontend/src/app/testQuests/academicLevel/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Sidebar from "@/components/Sidebar";
-// import Universidad from "@/components/Universidad";
+import Universidad from "@/components/Universidad";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
 import BotonSiguiente from "@/components/TestButton";
@@ -14,7 +14,7 @@ export default function AcademicLevel() {
     <div className="flex h-screen w-full bg-pink-900 text-white">
       <Sidebar isOpen={!isSidebarOpen} toggle={() => setSidebarOpen(!isSidebarOpen)} />
       <div className="flex flex-1 items-center justify-center bg-yellow-600">
-        {/* <Universidad /> */}
+        <Universidad />
       </div>
       <div>
         <BotonSiguiente respondido={true} />
