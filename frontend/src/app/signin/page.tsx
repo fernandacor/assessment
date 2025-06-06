6fe062e5064383138253703529843039e69de666
Alina Rosas <a01252720@itesm.mx>
2025-06-06 15:35:18 -0600

    medio signin

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,4 +1,3 @@
-//Sign In form
 "use client";
 
 import SignInForm from '@/components/SignInForm'
@@ -6,28 +5,38 @@ import Link from 'next/link'
 
 export default function SignIn() {
   return (
-    <div className='flex w-full flex-col px-6 py-12 bg-[url(/lightBG.jpg)] dark:bg-[url(/darkBG.jpg)] bg-cover h-screen' >
-      <div className='flex flex-col flex-initial w-3xl h-9/10 items-center bg-slate-50/50 rounded-xl mx-auto dark:bg-stone-700/25'>
-      <div className='mx-auto w-full max-w-sm'>
-        <h2 className='mt-10 text-center text-2xl font-light leading-9 tracking-tight text-lightest'>
-          Sign in to your account
-        </h2>
-      </div>
+    <div className="relative flex w-full flex-col px-6 py-12 bg-gray-50 dark:bg-[url(/darkBG.jpg)] bg-cover h-screen overflow-hidden">
+      
+      {/* Blobs */}
+      <div className="absolute top-0 left-25 w-72 h-72 bg-[#355C7D] rounded-full opacity-70 mix-blend-multiply filter blur-xl scale-200 hover:scale-250"></div>
+      <div className="absolute top-5 right-40 w-72 h-72 bg-[#A8E6CF] rounded-full opacity-70 mix-blend-multiply filter blur-xl scale-200 hover:scale-250"></div>
+      <div className="absolute -bottom-8 left-100 w-72 h-72 bg-[#a9b1e0] rounded-full opacity-70 mix-blend-multiply filter blur-xl scale-200 hover:scale-250"></div>
+      <div className="absolute bottom-40 left-20 w-72 h-72 bg-[#A8E6CF] rounded-full opacity-70 mix-blend-multiply filter blur-xl scale-200 hover:scale-250"></div>
+      <div className="absolute top-50 -right-20 w-72 h-72 bg-[#a9b1e0] rounded-full opacity-70 mix-blend-multiply filter blur-xl scale-200 hover:scale-250"></div>
 
-      <div className='mx-auto mt-10 w-full max-w-sm'>
-        <SignInForm />
 
-        <p className='mt-10 text-center text-sm text-gray-500'>
-          Not a member?{' '}
-          <Link
-            href='/auth/signup'
-            className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
-          >
-            Register now!
-          </Link>
-        </p>
+      {/* Sign-in card */}
+      <div className="relative flex flex-col flex-initial w-2xl h-2/3 mt-20 items-center bg-gray-100/50 rounded-xl mx-auto dark:bg-stone-700/25 z-10 shadow-lg backdrop-blur-md">
+        <div className="mx-auto w-full max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-light leading-9 tracking-tight text-lightest">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mx-auto mt-10 w-full max-w-sm">
+          <SignInForm />
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <Link
+              href="/auth/signup"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Register now!
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
